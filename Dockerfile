1c8ddb40a8f1d680419deee46ea5a5f38b32578a
Flook Peter <pflooky@hotmail.com>
2024-06-05 20:51:30 +0800

    Use different base of image depending on arch passed in via docker buildx

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
-FROM ubuntu:24.10
-
+FROM ubuntu:24.10 AS base
 ARG DUCKDB_VERSION=v1.0.0
-ARG ARCH=aarch64
 
+FROM base AS base-amd64
+ARG DUCKDB_ARCH=amd64
+
+FROM base AS base-arm64
+ARG DUCKDB_ARCH=aarch64
+
+FROM base-$TARGETARCH
 RUN apt-get update && apt-get install -y curl unzip
 
-RUN curl -L -o duckdb_cli.zip "https://github.com/duckdb/duckdb/releases/download/${DUCKDB_VERSION}/duckdb_cli-linux-${ARCH}.zip"
+RUN curl -L -o duckdb_cli.zip "https://github.com/duckdb/duckdb/releases/download/${DUCKDB_VERSION}/duckdb_cli-linux-${DUCKDB_ARCH}.zip"
 RUN unzip duckdb_cli.zip
 
 ENTRYPOINT [ "./duckdb" ]
--- docker_image.sh
+++ docker_image.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
+platforms="linux/amd64,linux/arm64"
+
 echo "Getting latest tag version from duckdb"
 duckdb_version=$(git -C build/duckdb describe --tags --abbrev=0)
 echo "Building for duckdb version: ${duckdb_version}"
 
-for i in amd64,amd64 arm64,aarch64 ; do
-  platform=${i%,*};
-  arch=${i#*,};
-  echo "Building docker image for platform: ${platform}, arch: ${arch}"
-  docker build \
-    --platform "${platform}" \
-    --build-arg "DUCKDB_VERSION=${duckdb_version}" \
-    --build-arg "ARCH=${arch}" \
-    -t datacatering/duckdb:"${duckdb_version}" --push .
-done
+docker run --privileged --rm tonistiigi/binfmt --install all
+docker buildx create --use --name builder
+docker buildx inspect --bootstrap builder
+
+docker buildx build \
+  --platform "$platforms" \
+  --build-arg "DUCKDB_VERSION=${duckdb_version}" \
+  -t datacatering/duckdb:"${duckdb_version}" --push .
 
 echo "Done!"
