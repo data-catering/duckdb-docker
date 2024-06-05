d09b1c6d21a82edbb6d42f11c0334f2e79f9228c
Flook Peter <pflooky@hotmail.com>
2024-06-05 20:42:32 +0800

    Don't use key value pairs in loop, add in docker login to github action

--- docker_image.sh
+++ docker_image.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
-declare -A platforms
-platforms[amd64]="amd64"
-platforms[arm64]="aarch64"
-
 echo "Getting latest tag version from duckdb"
 duckdb_version=$(git -C build/duckdb describe --tags --abbrev=0)
+echo "Building for duckdb version: ${duckdb_version}"
 
-for platform in "${!platforms[@]}"; do
-    echo ${platform} ${platforms[${platform}]}
-    echo "Building docker image for platform: ${platform}"
-    docker build \
-      --platform "${platform}" \
-      --build-arg "DUCKDB_VERSION=${duckdb_version}" \
-      --build-arg "ARCH=${platforms[${platform}]}" \
-      -t datacatering/duckdb:"${duckdb_version}" \
-      --push .
+for i in amd64,amd64 arm64,aarch64 ; do
+  platform=${i%,*};
+  arch=${i#*,};
+  echo "Building docker image for platform: ${platform}, arch: ${arch}"
+  docker build \
+    --platform "${platform}" \
+    --build-arg "DUCKDB_VERSION=${duckdb_version}" \
+    --build-arg "ARCH=${arch}" \
+    -t datacatering/duckdb:"${duckdb_version}" --push .
 done
 
 echo "Done!"
